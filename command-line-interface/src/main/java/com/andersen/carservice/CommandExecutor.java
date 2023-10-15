bf6f6eb572a6312586283c32d78ec9d51cf0fb2d
VadimBelyaev02 <vadimbelaev002@gmail.com>
2023-10-15 17:52:37 +0300

    Partially implemented class to execute commands

--- command-line-interface/src/main/java/com/andersen/carservice/CommandExecutor.java
+++ command-line-interface/src/main/java/com/andersen/carservice/CommandExecutor.java
@@ -1,20 +1,53 @@
 package com.andersen.carservice;
 
 import com.andersen.carservice.command.NamedCommand;
-import com.andersen.carservice.command.impl.AssignRepairers;
-import com.andersen.carservice.command.impl.CancelOrder;
-import com.andersen.carservice.command.impl.CompleteOrder;
+import com.andersen.carservice.command.impl.*;
+import com.andersen.carservice.storage.OrderStorage;
+import com.andersen.carservice.storage.RepairerStorage;
+import com.andersen.carservice.util.ArgumentParser;
+import lombok.AllArgsConstructor;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.Stream;
 
+@AllArgsConstructor
 public class CommandExecutor {
 
+    private final PrintWriter writer;
+    private final BufferedReader reader;
+    private final OrderStorage orderStorage;
+    private final RepairerStorage repairerStorage;
+    // Reader Writer
+
     private final List<NamedCommand> commands = Stream.of(
-            new AssignRepairers("assign-repairers"),
-            new CancelOrder("cancel-order"),
-            new CompleteOrder("complete-order")
+            new AssignRepairers("assign-repairers", orderStorage, repairerStorage),
+            new CancelOrder("cancel-order", orderStorage, repairerStorage),
+            new CompleteOrder("complete-order", orderStorage),
+            new FireRepairer("fire-repairer", repairerStorage, orderStorage),
+            new HireRepairer("hire-repairer", repairerStorage),
+            new ListOrders("list-orders", orderStorage),
+            new ListRepairers("list-repairers", repairerStorage),
+            new OpenOrder("open-order", orderStorage),
+            new ViewOrderInfo("view-order-info", orderStorage)
     ).toList();
 
+    public void start() {
+        while (true) {
+            try {
+                String command = reader.readLine();
 
+                if (Objects.equals(command, "exit")) {
+                    return;
+                }
+                List<String> arguments = ArgumentParser.parse(command);
+                commands.forEach(a -> a.execute(arguments, writer));
+            } catch (IOException e) {
+                writer.write(e.getMessage());
+            }
+        }
+    }
 }
