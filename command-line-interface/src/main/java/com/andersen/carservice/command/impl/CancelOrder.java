c290b4b6f7ea0a34e66b29fcd2ff879c1798c373
VadimBelyaev02 <vadimbelaev002@gmail.com>
2023-10-15 17:25:45 +0300

    Implemented cancel-order command

--- command-line-interface/src/main/java/com/andersen/carservice/command/impl/CancelOrder.java
+++ command-line-interface/src/main/java/com/andersen/carservice/command/impl/CancelOrder.java
@@ -1,22 +1,54 @@
 package com.andersen.carservice.command.impl;
 
-import com.andersen.carservice.command.NamedCommand;
+import com.andersen.carservice.command.FirstArgumentUuid;
+import com.andersen.carservice.entity.Order;
+import com.andersen.carservice.entity.Repairer;
+import com.andersen.carservice.storage.OrderStorage;
+import com.andersen.carservice.storage.RepairerStorage;
+import com.andersen.carservice.util.constants.OrderConstants;
+import com.andersen.carservice.util.constants.RepairerConstants;
 
-import java.io.OutputStream;
+import java.io.PrintWriter;
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
-public class CancelOrder extends NamedCommand {
-    public CancelOrder(String name) {
+public class CancelOrder extends FirstArgumentUuid {
+
+    private final OrderStorage orderStorage;
+    private final RepairerStorage repairerStorage;
+
+    public CancelOrder(String name, OrderStorage orderStorage, RepairerStorage repairerStorage) {
         super(name);
+        this.orderStorage = orderStorage;
+        this.repairerStorage = repairerStorage;
     }
 
     @Override
-    public void execute(List<String> arguments) {
-
+    protected void runCommand(List<String> arguments, PrintWriter writer) {
+        UUID orderId = UUID.fromString(arguments.get(1));
+        Optional<Order> orderOptional = orderStorage.findById(orderId);
+        orderOptional.ifPresentOrElse(
+                order -> {
+                    order.getRepairersIds().forEach(repairerId -> {
+                        Optional<Repairer> repairerOptional = repairerStorage.findById(repairerId);
+                        repairerOptional.ifPresentOrElse(
+                                repairer -> repairer.deleteOrder(orderId),
+                                () -> writer.println(RepairerConstants.notFoundById(repairerId))
+                        );
+                    });
+                    orderStorage.deleteById(orderId);
+                },
+                () -> writer.write(OrderConstants.notFoundById(orderId))
+        );
     }
 
-    @Override
-    public void printHelp(OutputStream output) {
 
+    @Override
+    public void printHelp(PrintWriter writer) {
+        writer.println("The command cancels one repairer. ");
+        writer.println("The first and the only one argument is the order's id. ");
+        writer.println("Format: cancel-order <order-id>");
+        writer.println("Example: cancel-order c7365c9e-3cf5-490f-9c85-38e936f758e6");
     }
 }
