b4ca54bf3f0cc89a02d741507ecb7add683627bb
VadimBelyaev02 <vadimbelaev002@gmail.com>
2023-10-15 17:32:12 +0300

    Implemented complete-order command

--- command-line-interface/src/main/java/com/andersen/carservice/command/impl/CompleteOrder.java
+++ command-line-interface/src/main/java/com/andersen/carservice/command/impl/CompleteOrder.java
@@ -1,22 +1,40 @@
 package com.andersen.carservice.command.impl;
 
-import com.andersen.carservice.command.NamedCommand;
+import com.andersen.carservice.command.NamedCommandWithFirstArgumentUuid;
+import com.andersen.carservice.entity.Order;
+import com.andersen.carservice.entity.enums.OrderStatus;
+import com.andersen.carservice.storage.OrderStorage;
+import com.andersen.carservice.util.constants.OrderConstants;
 
-import java.io.OutputStream;
+import java.io.PrintWriter;
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
-public class CompleteOrder extends NamedCommand {
-    public CompleteOrder(String name) {
+public class CompleteOrder extends NamedCommandWithFirstArgumentUuid {
+
+    private final OrderStorage orderStorage;
+
+    public CompleteOrder(String name, OrderStorage orderStorage) {
         super(name);
+        this.orderStorage = orderStorage;
     }
 
     @Override
-    public void execute(List<String> arguments) {
-
+    protected void runCommand(List<String> arguments, PrintWriter writer) {
+        UUID id = UUID.fromString(arguments.get(1));
+        Optional<Order> orderOptional = orderStorage.findById(id);
+        orderOptional.ifPresentOrElse(
+                order -> order.setStatus(OrderStatus.COMPLETED),
+                () -> writer.write(OrderConstants.notFoundById(id))
+        );
     }
 
     @Override
-    public void printHelp(OutputStream output) {
-
+    public void printHelp(PrintWriter writer) {
+        writer.println("The command completes one order. ");
+        writer.println("The first and the only one argument is the order's id. ");
+        writer.println("Format: complete-order <order-id>");
+        writer.println("Example: complete-order c7365c9e-3cf5-490f-9c85-38e936f758e6");
     }
 }
